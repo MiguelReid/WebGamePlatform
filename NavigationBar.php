26e38b5351317181455736def5b422320d7925b8
Miguel Reid Ruiz <mikeyreid2002@gmail.com>
2023-03-07 13:01:12 +0000

    Background image fits

--- NavigationBar.php
+++ NavigationBar.php
@@ -11,13 +11,16 @@
 </head>
 
 <body>
-    <div class="topnav">
-        <a href="/">
+    <div id="wrapper">
+        <div class="topnav" id="first">
             <a href="html-index.php" id="home">Home</a>
+            <img src="" class="img-fluid" id="image">
+        </div>
+        <div id="second">
             <a href="html-pairs.php" id="memory">Memory</a>
             <a href="html-leaderboard.php" id="leaderboard">Leaderboard</a>
             <a href="html-registration.php" id="register">Register</a>
-        </a>
+        </div>
     </div>
 </body>
 
--- html-index.php
+++ html-index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <style>
+        <?php include 'index.css'; ?>
+    </style>
 </head>
 <body>
     <?php include 'NavigationBar.php';?>
     <?php include 'index.php';?>
+    <div id="main">
 
-    
+    </div>
 </body>
 </html>
