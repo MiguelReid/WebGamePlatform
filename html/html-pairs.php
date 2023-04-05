13a26d86c92015fcd68d268043b7235e1cdc9aa4
Miguel Reid Ruiz <mikeyreid2002@gmail.com>
2023-04-05 21:29:26 +0200

    GridLayout for cards

--- html/html-pairs.php
+++ html/html-pairs.php
@@ -15,12 +15,35 @@
     <?php include 'NavigationBar.php'; ?>
 
     <div id="background">
-        <form>
-            <input type="button" id="playButton" value="Start the game" onclick=startGame()>
-        </form>
+        <div class="controls">
+            <button id="playButton" onclick=gameDifficulty()>Start the game</button>
+        </div>
+        <div id="board"></div>
+    </div>
+    </br>
+    <div id="stats">
+        <div id="moves">0 moves</div>
+        <div id="timer">time: 0 sec</div>
     </div>
 
+
+
     <script>
+
+        const values = {
+            board: document.getElementById('board'),
+            moves: document.getElementById('moves'),
+            timer: document.getElementById('timer'),
+            play: document.getElementById('playButton'),
+        }
+
+        const state = {
+            gameStarted: false,
+            flippedCards: 0,
+            totalFlips: 0,
+            totalTime: 0,
+            loop: null
+        }
 
         function getCookie(name) {
             var dc = document.cookie;
@@ -40,23 +63,64 @@
             return decodeURI(dc.substring(begin + prefix.length, end));
         }
 
-        function startGame() {
-            document.getElementById("playButton").style.display = 'none';
+        function shuffle(array) {
+            // Fisher-Yates shuffle
+            let currentIndex = array.length, randomIndex;
+
+            while (currentIndex != 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+                [array[currentIndex], array[randomIndex]] = [
+                    array[randomIndex], array[currentIndex]];
+            }
+            return array;
+        }
+
+        function gameDifficulty() {
+            document.getElementById('playButton').style.display = 'none';
             var difficulty = getCookie("difficulty");
             switch (difficulty) {
                 case "simple":
-                    // code block
+                    initializeGame(3, false);
                     break;
                 case "medium":
-                    // code block
+                    initializeGame(5, false);
                     break;
                 case "complex":
-                    // code block
+                    initializeGame(5, true);
                     break;
-                default:
-                // code block
             }
         }
+
+        function initializeGame(cards, flag) {
+            var eyes = ['closed', 'laughing', 'long', 'normal', 'rolling', 'winking'];
+            var mouth = ['open', 'sad', 'smiling', 'straight', 'surprise', 'teeth'];
+            var skin = ['green', 'red', 'yellow'];
+            var eyesS = shuffle(eyes).slice(0, cards);
+            var mouthS = shuffle(mouth).slice(0, cards);
+            var skinS = shuffle(skin).slice(0, cards);
+
+            if (cards == 3) {
+                values.board.style.gridTemplateColumns = 'repeat(3, 40px)';
+            }
+
+
+            for (var i = 0; i < cards * 2; i++) {
+                var img = document.createElement('img');
+                img.className = 'card';
+                img.id = 'img' + i;
+                img.src = '../resources/mouth/sad.png'
+                values.board.appendChild(img);
+
+            }
+
+            playGame();
+        }
+
+        function playGame() {
+
+        }
+
     </script>
 
 </body>
